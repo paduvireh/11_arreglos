4b122dc5df7b1c060adcde6b1a79853304a32ee8
paduvireh <paduvireh@unjbg.edu.pe>
2024-07-07 16:46:55 -0500

    Arreglos2: Se añadio los arreglos PAR e IMPAR

--- arreglos2.cpp
+++ arreglos2.cpp
@@ -5,12 +5,33 @@ int main(){
 	int n;
 	
 	cout<<"Ingrese la cantidad de elementos a leer: "; cin>>n;
-	int A[n];
+	int A[n], P[n], I[n];
+	int pcont=0, icont=0;
 	cout<<endl;
 	
 	for(int i=0; i<n; i++){
-		cout<<"Ingrese el elemento #"<<i+1<<": "; cin>>A[i];
+		cout<<"Ingrese el elemento #"<<i+1<<": "; 
+		cin>>A[i];
+		
+		if(A[i]%2==0){
+			P[pcont]=A[i];
+			pcont++;
+		}
+		else {
+			I[icont]=A[i];
+			icont++;
+		}	
 	}
-
+	
+	cout<<"\nELEMENTOS PARES: ";
+	for(int i=0; i<pcont; i++){
+		cout<<P[i]<<" ";
+	}
+	
+	cout<<"\nELEMENTOS IMPARES: ";
+	for(int i=0; i<icont; i++){
+		cout<<I[i]<<" ";
+	}
+	cout<<endl;
 	return 0;
 }
