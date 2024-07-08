bd0d212242624b509cd6ac2ad9c7fa9e113d4e2d
paduvireh <paduvireh@unjbg.edu.pe>
2024-07-07 19:47:53 -0500

    Se agrego arreglos4

---
+++ arreglos4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void multiplos(int A[], int n, int num){
+    for (int i=0; i<n; i++){
+        A[i]=num*(i+1);
+    }
+}
+
+int main(){
+    int n, num;
+    
+    cout<<"Ingrese el tamano del arreglo: "; cin>>n;
+    int A[n];
+    cout<<"Ingrese un numero: "; cin>>num;
+    
+    multiplos(A, n, num);
+    
+    cout<<"\nARREGLO PARA EL NUMERO "<<num<<": "<<endl;
+    for(int i=0; i<n; i++){
+        cout<<A[i]<<" ";
+    }
+    
+    cout<<endl;
+    return 0;
+}
+
